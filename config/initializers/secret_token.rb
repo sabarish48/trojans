8b07ccd527d82f5e9c7ede7cb533550df10e472f
sathish <sathishtop@gmail.com>
2011-09-29 23:51:36 +0530

    basic app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trojans::Application.config.secret_token = '1d6f43ba434489b7081be5135f0369fe3b181c41cae2950148103fa793844f8b686988a100f04dfc2cdacb19c15f0609e7dbb3e0262cbfe9e0351d5fea351499'
